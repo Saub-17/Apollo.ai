44b5d67b37cc314d1c6a92d6355e7fe128868876
Sahil Somil <107985666+SILENTswami@users.noreply.github.com>
2024-08-27 22:54:25 +0530

    Delete Apollo directory

--- Apollo/backend/semanticAnalysis.py
+++
@@ -1,9 +0,0 @@
-# Emotions: 'Joy', 'Sadness', 'Anger', 'Love', 'Fear', 'Surprise'
-
-from transformers import pipeline
-
-emotion_classifier = pipeline("text-classification", model="bhadresh-savani/distilbert-base-uncased-emotion")
-
-def analyze_sentiment(text):
-    result = emotion_classifier(text)
-    return result
